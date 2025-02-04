020f25395377c7929fba4b266b6faf9ceb5e3b14
Seyfulla1 <160208902+Seyfulla1@users.noreply.github.com>
2025-02-04 20:47:12 +0400

    Created families using all of the constructors of Human class

--- lesson03/Main.java
+++ lesson03/Main.java
@@ -1,8 +1,15 @@
+
+
 public class Main {
     public static void main(String[] args) {
-        Pet dog=new Pet("dog","Rock",5,75,new String[]{"eat","drink","sleep"});
-        Human human=new Human("Michael","Karleone",1977,90,dog,null,null);
-        System.out.println(dog);
-        System.out.println(human);
+    Human loretta=new Human("Loretta","Stinson",1948);
+    Human jerome=new Human("Jerome","Whittaker",1940);
+    Human barney=new Human("Barney","Stinson",1975,loretta,jerome);
+    Human scherbatskySr=new Human("Sr","Scherbatsky",1941);
+    Human genevieve=new Human("Genevieve","Scherbatsky",1945);
+    Pet dog=new Pet("Bulldog","bd",6,66,new String[]{"eat","sleep","bark","drink"});
+    Human robin=new Human("Robin","Scherbatsky",1980,90,dog,genevieve,scherbatskySr);
+    System.out.println(robin);
+
     }
 }
