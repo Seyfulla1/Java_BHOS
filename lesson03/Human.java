25c99303b64ef369abee7effbe3300b82b9a2133
Seyfulla1 <160208902+Seyfulla1@users.noreply.github.com>
2025-02-06 22:47:10 +0400

    Added schedule instance variable to Human class and modified constructors accordingly

--- lesson03/Human.java
+++ lesson03/Human.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Human {
     private String name;
     private String surname;
@@ -6,6 +8,7 @@ public class Human {
     private Pet pet;
     private Human mother;
     private Human father;
+    private String[][] schedule;
     public Human()
     {
         this.name="Not";
@@ -15,6 +18,7 @@ public class Human {
         this.pet=null;
         this.mother=null;
         this.father=null;
+        this.schedule=new String[][]{{"N/A","N/A"}};
     }
     public Human(String name,String surname,int year)
     {
@@ -25,6 +29,7 @@ public class Human {
         this.pet=new Pet();
         this.mother=new Human();
         this.father=new Human();
+        this.schedule=new String[][]{{"N/A","N/A"}};
     }
     public Human(String name,String surname,int year, Human mother, Human father)
     {
@@ -35,10 +40,11 @@ public class Human {
         this.father=father;
         this.iq=-1;
         this.pet=new Pet();
+        this.schedule=new String[][]{{"N/A","N/A"}};
 
 
     }
-    public Human(String name, String surname, int year, int iq, Pet pet, Human mother, Human father) {
+    public Human(String name, String surname, int year, int iq, Pet pet, Human mother, Human father,String[][] schedule) {
         this.name = name;
         this.surname = surname;
         this.year = year;
@@ -46,6 +52,7 @@ public class Human {
         this.pet = pet;
         this.mother = mother;
         this.father = father;
+        this.schedule = schedule;
 
 
     }
@@ -71,6 +78,25 @@ public class Human {
         System.out.println("I have a "+pet.getSpecies()+". It is "
                 +pet.getAge()+" years old, he is "+isSly);
     }
+    public boolean feedPet(boolean isFeedingTime)
+    {
+
+        if(!isFeedingTime)
+        {
+            Random rand = new Random();
+            int randomNum = rand.nextInt(101);
+            if(pet.getTrickLevel()>randomNum)
+            {
+                isFeedingTime=true;
+            }
+
+        }
+        String output=isFeedingTime?"Hm... I will feed "+name+"'s "+pet.getName():"I think "+name+"'s "+pet.getName()+" is not hungry.";
+        System.out.println(output);
+        return isFeedingTime;
+
+    }
+
     @Override
     public String toString() {
         return "Human{name='"+name+"', surname='"+surname+"', year="+year+", iq="+iq+
