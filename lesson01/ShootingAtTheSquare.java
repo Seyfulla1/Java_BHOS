6e348e25c4f394cb7ec09f4246dbf7df56b8f911
Seyfulla1 <160208902+Seyfulla1@users.noreply.github.com>
2025-02-08 09:53:47 +0400

    Incompletely implemented area shooting feature

--- lesson01/ShootingAtTheSquare.java
+++ lesson01/ShootingAtTheSquare.java
@@ -5,13 +5,43 @@ import java.util.Scanner;
 public class ShootingAtTheSquare {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        Scanner sc_int= new Scanner(System.in);
         Random rand = new Random();
         int target_row = rand.nextInt(5);
         int target_col = rand.nextInt(5);
         int row;
         int col;
         String[][] board= new String[5][5];
+        int[][] targets=new int[3][2];
+        int rowOrCol=rand.nextInt(2);
+        int constantPoint=rand.nextInt(5);
+        int random_index=rand.nextInt(3);
+        for(int i=0;i<3;i++){
+            if(rowOrCol==0){
+                targets[i][0]=constantPoint;
+                targets[i][1]=random_index++;
+            }
+            else {
+                targets[i][1]=constantPoint;
+                targets[i][0]=random_index++;
+            }
+        }
+        for(int i=0;i<3;i++){
+            System.out.println(targets[i][0]+" "+targets[i][1]);
+        }
+
         initBoard(board);
+        System.out.println("All set. Get ready to rumble!");
+        System.out.println("Which mode do you want to play:\n1.Normal\n2.Area shooting");
+        int mode = sc_int.nextInt();
+
+        int shooting_needed=0;
+        int counter=0;
+        switch(mode){
+            case 1:shooting_needed=1;break;
+            case 2:shooting_needed=3;break;
+            default:System.out.println("Invalid mode");
+        }
         while(true){
             printBoard(board);
             while(true){
@@ -31,8 +61,15 @@ public class ShootingAtTheSquare {
                 }
             }
             if(row==target_row&&col==target_col){
-                board[row][col]="x";
-                break;
+                if(!(board[row][col].equals("x")))
+                {
+                    board[row][col]="x";
+                    counter++;
+                    if(counter==shooting_needed)
+                    {
+                        break;
+                    }
+                }
             }
             else {
                 board[row][col]="*";
