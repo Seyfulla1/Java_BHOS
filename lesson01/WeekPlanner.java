2fd0268eea3dff54ec7c25ea7cbccfb4ea6a87ce
Seyfulla1 <160208902+Seyfulla1@users.noreply.github.com>
2025-02-08 12:51:46 +0400

    Implemented advanced functionality

--- lesson01/WeekPlanner.java
+++ lesson01/WeekPlanner.java
@@ -8,6 +8,18 @@ public class WeekPlanner {
         schedule[0][1]="do homework";
         schedule[1][0]="Monday";
         schedule[1][1]="go to courses; watch a film";
+        schedule[2][0]="Tuesday";
+        schedule[2][1]="play football";
+        schedule[3][0]="Wednesday";
+        schedule[3][1]="read a book";
+        schedule[4][0]="Thursday";
+        schedule[4][1]="doing nothing";
+        schedule[5][0]="Friday";
+        schedule[5][1]="blaabla";
+        schedule[6][0]="Saturday";
+        schedule[6][1]="partying";
+
+
         boolean wantToExit = false;
         while(!wantToExit)
         {
@@ -15,16 +27,52 @@ public class WeekPlanner {
             String day = sc.nextLine().trim().toUpperCase();
             switch(day){
                 case "SUNDAY":
-                    System.out.println("Your text for Sunday: "+schedule[1][1]);
+                    System.out.println("Your text for Sunday: "+schedule[0][1]);
                     break;
                     case "MONDAY":
-                        System.out.println("Your text for Monday: "+schedule[0][1]);
+                        System.out.println("Your text for Monday: "+schedule[1][1]);
                         break;
-                        case "EXIT":
-                            wantToExit = true;
+                        case "TUESDAY":
+                            System.out.println("Your text for Tuesday: "+schedule[2][1]);
                             break;
-                            default:
-                                System.out.println("Soory, I don't understand you, please try again.");
+                            case "WEDNESDAY":
+                                System.out.println("Your text for Wednesday: "+schedule[3][1]);
+                                break;
+                                case "THURSDAY":
+                                    System.out.println("Your text for Thursday: "+schedule[4][1]);
+                                    break;
+                                    case "FRIDAY":
+                                        System.out.println("Your text for Friday: "+schedule[5][1]);
+                                        break;
+                                        case "SATURDAY":
+                                            System.out.println("Your text for Saturday: "+schedule[6][1]);
+                                            break;
+                                            case "EXIT":
+                                                wantToExit = true;
+                                                break;
+                                            default:
+                                                if(day.startsWith("CHANGE")||day.startsWith("RESCHEDULE"))
+                                                {
+                                                    String dayToChange = day.substring(day.indexOf(" ")+1);
+                                                    int i;
+                                                    for(i=0; i< schedule.length; i++)
+                                                    {
+                                                        if(schedule[i][0].toUpperCase().equals(dayToChange))
+                                                        {
+                                                            System.out.println("Please, input the new tasks for "+schedule[i][0]);
+                                                            schedule[i][1]=sc.nextLine();
+                                                            break;
+                                                        }
+                                                    }
+                                                    if(i== schedule.length)
+                                                    {
+                                                        System.out.println("Sorry, I don't understand you, please try again.");
+                                                    }
+
+                                                }else
+                                                {
+                                                    System.out.println("Sorry, I don't understand you, please try again.");
+                                                }
 
             }
             if(wantToExit)
