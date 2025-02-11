7905537be31a28cbb467c92625cfc2c6f3e10288
Seyfulla1 <160208902+Seyfulla1@users.noreply.github.com>
2025-02-11 18:05:24 +0400

    Made minor adjustments to constructors and to the toString method

--- src/main/java/az/edu/bhos/lesson05/Pet.java
+++ src/main/java/az/edu/bhos/lesson05/Pet.java
@@ -1,5 +1,7 @@
 package az.edu.bhos.lesson05;
 
+import java.util.Arrays;
+
 public class Pet {
     private String species;
     private String nickname;
@@ -12,14 +14,14 @@ public class Pet {
         this.nickname = "";
         this.age = -1;
         this.trickLevel = -1;
-        this.habits = new String[]{"N/A"};
+        this.habits = new String[0];
     }
     public Pet(String species, String nickname) {
         this.species = species;
         this.nickname = nickname;
         this.age = -1;
         this.trickLevel = -1;
-        this.habits = new String[]{"N/A"};
+        this.habits = new String[0];
     }
     public Pet(String species, String nickname, int age, int trickLevel, String[] habits) {
         this.species = species;
@@ -35,12 +37,10 @@ public class Pet {
     public String getNickName() {
         return nickname;
     }
-    public int getAge()
-    {
+    public int getAge() {
         return age;
     }
-    public int getTrickLevel()
-    {
+    public int getTrickLevel() {
         return trickLevel;
     }
     private String[] getHabits(){return habits;}
@@ -49,8 +49,7 @@ public class Pet {
     private void setAge(int age){this.age = age;}
     private void setTrickLevel(int trickLevel){this.trickLevel = trickLevel;}
     private void setHabits(String[] habits){this.habits = habits;}
-    public void eat()
-    {
+    public void eat() {
         System.out.println("I am eating");
     }
     public void respond() {
@@ -60,21 +59,9 @@ public class Pet {
     public void foul() {
         System.out.println("I need to cover it up");
     }
-    public String formatHabits() {
-        String content = "[";
-        int len=habits.length;
-        for (int i=0;i<len-1;i++)
-        {
-            content+=habits[i]+", ";
-        }
-        content+=habits[len-1]+"]";
-        return content;
-    }
-
     @Override
     public String toString() {
-
-        return species+"{nickname='" + nickname +"', age="+age+", trickLevel="+trickLevel+", habits="+formatHabits()+"}";
+        return species+"{nickname='" + nickname +"', age="+age+", trickLevel="+trickLevel+", habits="+ Arrays.toString(habits)+"}";
     }
 
 
