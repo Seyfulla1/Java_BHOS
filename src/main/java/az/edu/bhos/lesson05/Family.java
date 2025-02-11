55b8d139cc1291cdc847f63ea7c41b2b5c6a3406
Seyfulla1 <160208902+Seyfulla1@users.noreply.github.com>
2025-02-11 14:26:33 +0400

    Implemented getters and setters and overrode toString method

--- src/main/java/az/edu/bhos/lesson05/Family.java
+++ src/main/java/az/edu/bhos/lesson05/Family.java
@@ -1,8 +1,38 @@
 package az.edu.bhos.lesson05;
+import java.util.Arrays;
 
 public class Family {
     private Human mother;
     private Human father;
     private Human[] children;
     private Pet pet;
+
+    public Human getMother() {
+        return mother;
+    }
+    public Human getFather() {
+        return father;
+    }
+    public Human[] getChildren() {
+        return children;
+    }
+    public Pet getPet() {
+        return pet;
+    }
+    public void setMother(Human mother) {
+        this.mother = mother;
+    }
+    public void setFather(Human father) {
+        this.father = father;
+    }
+    public void setChildren(Human[] children) {
+        this.children = children;
+    }
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+    @Override
+    public String toString() {
+        return "Family{mother="+mother.getName()+" father="+father.getName()+" children="+ Arrays.toString(children)+" pet="+pet.toString()+"}";
+    }
 }
