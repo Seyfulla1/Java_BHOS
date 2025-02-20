7b1c68a9172ec586cbd6ae976873bc05d66ea0a6
Seyfulla1 <160208902+Seyfulla1@users.noreply.github.com>
2025-02-20 17:47:28 +0400

    Initial commit

---
+++ src/main/java/az/edu/bhos/lesson07/Main.java
@@ -0,0 +1,51 @@
+package az.edu.bhos.lesson07;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+    Human loretta=new Human("Loretta","Stinson",1948);
+    Human jerome=new Human("Jerome","Whittaker",1940);
+    Family family1=new Family(loretta,jerome);
+
+    //-------------------------------------------------------------------
+
+    Human genevieve=new Human("Genevieve","Scherbatsky",1945);
+    Human scherbatskySr=new Human("Sr","Scherbatsky",1941);
+    Family family2=new Family(genevieve,scherbatskySr);
+
+    //--------------------------------------------------------------------
+
+    String[][] barneySchedule={
+            {"Monday","drink"},
+            {"Wednesday","drink more"},
+            {"Friday","party"},
+    };
+    Human barney=new Human("Barney","Stinson",1975);
+    barney.setSchedule(barneySchedule);
+    String[][] robinSchedule={
+            {"Monday","Drawing"},
+            {"Wednesday","Playing badminton"},
+            {"Friday","Reading books"},
+            {"Saturday","Partying"}
+    };
+    Human robin=new Human("Robin","Scherbatsky",1980,90,robinSchedule);
+    Human ted=new Human("Ted","Mosby",1978);
+    Pet dog=new Pet("Bulldog","bd",6,66,new String[]{"eat","sleep","bark","drink"});
+    family2.addChild(robin);
+    family2.addChild(ted);
+    family2.addChild(barney);
+        System.out.println(family2);
+        System.out.println("----------------------------");
+        family2.deleteChild(new Human("Ted","Mosby",1978));
+        System.out.println(family2);
+
+
+
+
+
+
+
+
+    }
+}
---
+++ src/main/java/az/edu/bhos/lesson07/Pet.java
@@ -0,0 +1,106 @@
+package az.edu.bhos.lesson07;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+public class Pet {
+    private String species;
+    private String nickname;
+    private int age;
+    private int trickLevel;
+    private String[] habits;
+
+    static{
+        System.out.println("Pet class is loaded.");
+    }
+    {
+        System.out.println("A new Pet object is created.");
+    }
+
+    public Pet() {
+        this.species = "N/A";
+        this.nickname = "N/A";
+        this.age = -1;
+        this.trickLevel = -1;
+        this.habits = new String[0];
+    }
+    public Pet(String species, String nickname) {
+        this.species = species;
+        this.nickname = nickname;
+        this.age = -1;
+        this.trickLevel = -1;
+        this.habits = new String[0];
+    }
+    public Pet(String species, String nickname, int age, int trickLevel, String[] habits) {
+        this.species = species;
+        this.nickname = nickname;
+        this.age = age;
+        this.trickLevel = trickLevel;
+        this.habits = habits;
+
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+    public String getNickName() {
+        return nickname;
+    }
+    public int getAge() {
+        return age;
+    }
+    public int getTrickLevel() {
+        return trickLevel;
+    }
+    private String[] getHabits(){return habits;}
+
+    private void setSpecies(String species){this.species = species;}
+    private void setNickname(String nickname){this.nickname = nickname;}
+    private void setAge(int age){this.age = age;}
+    private void setTrickLevel(int trickLevel){this.trickLevel = trickLevel;}
+    private void setHabits(String[] habits){this.habits = habits;}
+
+    public void eat() {
+        System.out.println("I am eating");
+    }
+    public void respond() {
+        System.out.println("Hello, owner. I am - "+nickname+". I miss you!");
+
+    }
+    public void foul() {
+        System.out.println("I need to cover it up");
+    }
+
+    @Override
+    public boolean equals(Object that) {
+        if (this == that) {
+            return true;
+        }
+        if(that==null || this.getClass()!=that.getClass()) {
+            return false;
+        }
+        Pet thatPet = (Pet)that;
+        return Objects.equals(species, thatPet.species) &&
+                Objects.equals(nickname, thatPet.nickname) &&
+                age==thatPet.age && trickLevel==thatPet.trickLevel;
+    }
+    @Override
+    public int hashCode() {
+        int result=17;
+        result = 31 * result + species.hashCode();
+        result = 31 * result + nickname.hashCode();
+        result = 31 * result + age;
+        result = 31 * result + trickLevel;
+        return result;
+    }
+    @Override
+    public String toString() {
+        return species+"{nickname='" + nickname +"', age="+age+
+                ", trickLevel="+trickLevel+
+                ", habits="+ Arrays.toString(habits)+"}";
+    }
+
+
+
+
+}
