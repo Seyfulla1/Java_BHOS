7d4870ab4b89f839b27ab4e8fbbe0653f10dc059
Seyfulla1 <160208902+Seyfulla1@users.noreply.github.com>
2025-02-21 19:39:45 +0400

    testToString() for Pet class

--- src/test/java/az/edu/bhos/lesson07/PetTest.java
+++ src/test/java/az/edu/bhos/lesson07/PetTest.java
@@ -1,4 +1,14 @@
 package az.edu.bhos.lesson07;
 
-public class PetTest {
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class PetTest {
+
+    @Test
+    void testToString() {
+        Pet test=new Pet(Species.DOG,"Felipe Melo",41,99,new String[]{"cry","bark","lose"});
+        String expected="DOG{nickname='Felipe Melo', age=41, trickLevel=99, habits=[cry, bark, lose]}";
+        assertEquals(expected,test.toString());
+    }
 }
