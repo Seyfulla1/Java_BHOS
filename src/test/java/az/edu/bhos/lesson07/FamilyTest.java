f735cb371e3426b0ed3d5cef52985e47765b0f6b
Seyfulla1 <160208902+Seyfulla1@users.noreply.github.com>
2025-02-21 21:51:58 +0400

    Implemented required unit tests for Family class methods

--- src/test/java/az/edu/bhos/lesson07/FamilyTest.java
+++ src/test/java/az/edu/bhos/lesson07/FamilyTest.java
@@ -1,4 +1,89 @@
 package az.edu.bhos.lesson07;
 
-public class FamilyTest {
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FamilyTest {
+
+   static Human mother;
+   static Human father;
+   static Family family;
+   static String[][] schedule1;
+   static Human child1;
+   static Human child2;
+   static Human child3;
+
+    @BeforeEach
+    void setUp()
+    {
+         mother=new Human("Mother","Motherov",1970);
+         father=new Human("Father","Fatherov",1965);
+         family=new Family(mother,father);
+         schedule1=new String[][]{{DayOfWeek.MONDAY.name(),"Doing Monday things"},{DayOfWeek.WEDNESDAY.name(),"Doing wednesday things"},{DayOfWeek.FRIDAY.name(),"Friday night hobby"},{DayOfWeek.SATURDAY.name(),"Partying"}};
+         child1=new Human("Child1","Childov1",1995,100,schedule1);
+         child2=new Human("Child2","Childov2",1997);
+         child3=new Human("Child3","Childov3",1999);
+
+
+    }
+
+    @Test
+    void testAddChild() {
+        int originalChildrenCount=family.getChildren().length;
+        family.addChild(child1);
+        assertEquals(originalChildrenCount+1,family.getChildren().length);
+        assertEquals(child1.getFamily(),family);
+        assertEquals(family.getChildren()[family.countFamily()-3],child1);
+    }
+
+    @Test
+    void testDeleteChild() {
+        family.addChild(child1);
+        family.addChild(child2);
+        family.addChild(child3);
+        int originalChildrenCount=family.getChildren().length;
+        family.deleteChild(child1);
+        assertEquals(originalChildrenCount-1,family.getChildren().length);
+        assertEquals(family.getChildren()[0],child2);
+        assertEquals(family.getChildren()[1],child3);
+        assertNull(child1.getFamily());
+        family.addChild(child1);
+        family.deleteChild(child3);
+        assertEquals(family.getChildren()[0],child2);
+        assertEquals(family.getChildren()[1],child1);
+        originalChildrenCount=family.getChildren().length;
+        family.deleteChild(child3);
+        assertEquals(originalChildrenCount,family.getChildren().length);
+
+        
+    }
+
+
+    @Test
+    void testCountFamily() {
+        assertEquals(2,family.countFamily());
+        family.addChild(child1);
+        assertEquals(3,family.countFamily());
+        family.addChild(child2);
+        assertEquals(4,family.countFamily());
+    }
+
+    @Test
+    void testToString() {
+        family.addChild(child1);
+        family.addChild(child2);
+        family.setPet(new Pet(Species.DOG,"bd",6,66,new String[]{"eat","sleep","bark","drink"}));
+        String expected="Family{\n" +
+                "mother=Human{name='Mother', surname='Motherov', year=1970, iq=-1, schedule=[]}\n" +
+                "father=Human{name='Father', surname='Fatherov', year=1965, iq=-1, schedule=[]}\n" +
+                "children=[Human{name='Child1', surname='Childov1', " +
+                "year=1995, iq=100, schedule=[[MONDAY, Doing Monday things], [WEDNESDAY, Doing wednesday things], [FRIDAY, Friday night hobby], [SATURDAY, Partying]]}, " +
+                "Human{name='Child2', surname='Childov2', year=1997, iq=-1, schedule=[]}]\n" +
+                "pet=DOG{nickname='bd', age=6, trickLevel=66, habits=[eat, sleep, bark, drink]}\n" + "}";
+        assertEquals(expected,family.toString());
+
+    }
 }
