e1c6e772fe439debc9c891ed2f984cbffc9ff4ab
Seyfulla1 <160208902+Seyfulla1@users.noreply.github.com>
2025-02-21 19:33:53 +0400

    testToString() for Human class

--- src/test/java/az/edu/bhos/lesson07/HumanTest.java
+++ src/test/java/az/edu/bhos/lesson07/HumanTest.java
@@ -1,4 +1,15 @@
 package az.edu.bhos.lesson07;
 
-public class HumanTest {
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class HumanTest {
+
+    @Test
+    void testToString() {
+        String[][] testSchedule={{DayOfWeek.MONDAY.name(),"Dancing"},{DayOfWeek.WEDNESDAY.name(),"football"},{DayOfWeek.SATURDAY.name(),"Partying"}};
+        Human test=new Human("Ali","Helehe",1990,990,testSchedule);
+        String expected="Human{name='Ali', surname='Helehe', year=1990, iq=990, schedule=[[MONDAY, Dancing], [WEDNESDAY, football], [SATURDAY, Partying]]}";
+        assertEquals(expected,test.toString());
+    }
 }
