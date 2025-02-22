e6185597844b4b04960db06712576ce7b1519d33
Seyfulla1 <160208902+Seyfulla1@users.noreply.github.com>
2025-02-22 13:32:30 +0400

    Introduced a constructor to specify parameters for a pet

---
+++ src/main/java/az/edu/bhos/lesson07/Species.java
@@ -0,0 +1,19 @@
+package az.edu.bhos.lesson07;
+
+public enum Species {
+    DOG(false, 4, true),
+    CAT(false, 4, true),
+    FISH(false, 0, false),
+    BIRD(true, 2, false),
+    NO_SPECIES(false,-1,false);
+
+    final boolean canFly;
+    final int numberOfLegs;
+    final boolean hasFur;
+    Species(boolean canFly, int numberOfLegs, boolean hasFur) {
+        this.canFly = canFly;
+        this.numberOfLegs = numberOfLegs;
+        this.hasFur = hasFur;
+    }
+
+}
