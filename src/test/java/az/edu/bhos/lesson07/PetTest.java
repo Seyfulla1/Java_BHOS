96f08cbbd6ff8e954b8e4709f3e6a92bed3dc3cc
Seyfulla1 <160208902+Seyfulla1@users.noreply.github.com>
2025-02-22 13:35:04 +0400

    Modified testToString method according to the needs

--- src/test/java/az/edu/bhos/lesson07/PetTest.java
+++ src/test/java/az/edu/bhos/lesson07/PetTest.java
@@ -8,7 +8,7 @@ class PetTest {
     @Test
     void testToString() {
         Pet test=new Pet(Species.DOG,"Felipe Melo",41,99,new String[]{"cry","bark","lose"});
-        String expected="DOG{nickname='Felipe Melo', age=41, trickLevel=99, habits=[cry, bark, lose]}";
+        String expected="DOG{nickname='Felipe Melo', age=41, trickLevel=99, habits=[cry, bark, lose], characteristics=can't fly, has 4 legs, has fur}";
         assertEquals(expected,test.toString());
     }
 }
