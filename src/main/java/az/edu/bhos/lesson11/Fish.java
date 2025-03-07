9227fabe31f75fee7e9b0ef0291aebd90a5d07e1
Seyfulla1 <160208902+Seyfulla1@users.noreply.github.com>
2025-03-07 19:50:29 +0400

    Reapply "Removed pet instances"
    This reverts commit ddb120e82fd655a1a7f0cc7a1e998b1ccddaf8c2.

---
+++ src/main/java/az/edu/bhos/lesson11/Fish.java
@@ -0,0 +1,8 @@
+package az.edu.bhos.lesson11;
+
+public class Fish extends Pet{
+    @Override
+    public void respond() {
+        System.out.println("Fish is swimming");
+    }
+}
---
+++ src/main/java/az/edu/bhos/lesson11/RoboCat.java
@@ -0,0 +1,8 @@
+package az.edu.bhos.lesson11;
+
+public class RoboCat extends Pet{
+    @Override
+    public void respond() {
+        System.out.println("RoboCat is meowing worse than DomesticCat");
+    }
+}
