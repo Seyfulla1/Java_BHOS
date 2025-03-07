fab70871d6d710ef553d6fe24f0dfb225aa20a50
Seyfulla1 <160208902+Seyfulla1@users.noreply.github.com>
2025-03-07 19:57:04 +0400

    Removed incorrectly implemented files

--- src/main/java/az/edu/bhos/lesson11/Fish.java
+++
@@ -1,8 +0,0 @@
-package az.edu.bhos.lesson11;
-
-public class Fish extends Pet{
-    @Override
-    public void respond() {
-        System.out.println("Fish is swimming");
-    }
-}
--- src/main/java/az/edu/bhos/lesson11/RoboCat.java
+++
@@ -1,8 +0,0 @@
-package az.edu.bhos.lesson11;
-
-public class RoboCat extends Pet{
-    @Override
-    public void respond() {
-        System.out.println("RoboCat is meowing worse than DomesticCat");
-    }
-}
