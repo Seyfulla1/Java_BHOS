c517108be1efa8213b42c5b2ddc22cda2d699549
Seyfulla1 <160208902+Seyfulla1@users.noreply.github.com>
2025-03-07 21:18:21 +0400

    Overrode greetPet() and implemented a unique class method

--- src/main/java/az/edu/bhos/lesson11/Man.java
+++ src/main/java/az/edu/bhos/lesson11/Man.java
@@ -1,4 +1,29 @@
 package az.edu.bhos.lesson11;
 
-public final class Man {
+public final class Man extends Human {
+    final String gender;
+    {
+        gender="Male";
+    }
+    public Man()
+    {
+        super("Not", "Applicable", -1, -1, new String[0][0]);
+    }
+    public Man(String name, String surname, int year)
+    {
+        super(name, surname, year, -1, new String[0][0]);
+    }
+    public Man(String name, String surname, int year, int iq, String[][] schedule)
+    {
+        super(name, surname, year, iq, schedule);
+    }
+    @Override
+    public void greetPet()
+    {
+        System.out.println("Hello, " + getFamily().getPet().getNickName());
+    }
+    public void repairCar()
+    {
+        System.out.println("I'm repairing my wife's car, as, apparently, i'm the only one who can do it.");
+    }
 }
--- src/main/java/az/edu/bhos/lesson11/Woman.java
+++ src/main/java/az/edu/bhos/lesson11/Woman.java
@@ -1,4 +1,24 @@
 package az.edu.bhos.lesson11;
 
-public final class Woman {
+public final class Woman extends Human {
+    final String gender;
+    {
+        gender="Female";
+    }
+    public Woman() {
+        super();
+    }
+    public Woman(String name, String surname, int year)
+    {
+        super(name, surname, year);
+    }
+    @Override
+    public void greetPet()
+    {
+        System.out.println("Hello, " + getFamily().getPet().getNickName()+", but i can't drive.");
+    }
+    public void makeUp()
+    {
+        System.out.println("I'm making up however i want, because i'm strong and independent");
+    }
 }
