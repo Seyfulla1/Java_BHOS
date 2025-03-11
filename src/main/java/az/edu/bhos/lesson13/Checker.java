0a2bb72fef4a906a80d80b955642aa599f889bcd
Seyfulla1 <160208902+Seyfulla1@users.noreply.github.com>
2025-03-11 11:48:58 +0400

    Initial commit

---
+++ src/main/java/az/edu/bhos/lesson13/Checker.java
@@ -0,0 +1,13 @@
+package az.edu.bhos.lesson13;
+
+import java.util.Comparator;
+
+public class Checker implements Comparator<Player> {
+    @Override
+    public int compare(Player a, Player b){
+        if(a.getScore() == b.getScore()){
+            return a.getName().compareTo(b.getName());
+        }
+        return Integer.compare(a.getScore(), b.getScore());
+    }
+}
---
+++ src/main/java/az/edu/bhos/lesson13/Player.java
@@ -0,0 +1,18 @@
+package az.edu.bhos.lesson13;
+
+public class Player {
+    private String name;
+    private int score;
+
+    public Player(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public int getScore() {
+        return score;
+    }
+}
