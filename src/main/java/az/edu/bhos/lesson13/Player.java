db4caeb17eadb8e2129c75ad3408fb9f80ce02f4
Seyfulla1 <160208902+Seyfulla1@users.noreply.github.com>
2025-03-11 12:00:37 +0400

    Made String name and int score final

--- src/main/java/az/edu/bhos/lesson13/Player.java
+++ src/main/java/az/edu/bhos/lesson13/Player.java
@@ -1,8 +1,8 @@
 package az.edu.bhos.lesson13;
 
 public class Player {
-    private String name;
-    private int score;
+    private final String name;
+    private final int score;
 
     public Player(String name, int score) {
         this.name = name;
@@ -14,5 +14,9 @@ public class Player {
     }
     public int getScore() {
         return score;
+    }
+    @Override
+    public String toString() {
+        return name + " " + score;
     }
 }
