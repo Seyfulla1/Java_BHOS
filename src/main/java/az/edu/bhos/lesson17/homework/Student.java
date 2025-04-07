24887434289bf5b36d17c02cac92fc2ba07d9c5f
Seyfulla1 <160208902+Seyfulla1@users.noreply.github.com>
2025-04-07 23:14:59 +0400

    Added fields and constructor

--- src/main/java/az/edu/bhos/lesson17/homework/Student.java
+++ src/main/java/az/edu/bhos/lesson17/homework/Student.java
@@ -1,4 +1,16 @@
 package az.edu.bhos.lesson17.homework;
 
+import java.util.ArrayList;
+
 public class Student extends Human implements Gradable {
+    private double scholarship;
+    private int studentID;
+    private ArrayList<Course> coursesTaken;
+    public Student(String name, String surname, int age, double scholarship, int studentID) {
+        super(name, surname, age);
+        this.scholarship = scholarship;
+        this.studentID = studentID;
+        this.coursesTaken = new ArrayList<>();
+    }
+
 }
--- src/main/java/az/edu/bhos/lesson17/homework/Teacher.java
+++ src/main/java/az/edu/bhos/lesson17/homework/Teacher.java
@@ -1,4 +1,13 @@
 package az.edu.bhos.lesson17.homework;
 
 public class Teacher extends Human implements Teachable {
+    private double salary;
+    private int teacherID;
+    private Course courseTaught;
+    public Teacher(String name, String surname, int age, double salary, int teacherID, Course courseTaught) {
+        super(name, surname, age);
+        this.salary = salary;
+        this.teacherID = teacherID;
+        this.courseTaught = courseTaught;
+    }
 }
