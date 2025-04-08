4776d574f73bf60461500055686fc04d33600433
Seyfulla1 <160208902+Seyfulla1@users.noreply.github.com>
2025-04-08 16:08:55 +0400

    Added code to test the program

---
+++ src/main/java/az/edu/bhos/lesson17/homework/Main.java
@@ -0,0 +1,47 @@
+package az.edu.bhos.lesson17.homework;
+
+public class Main {
+    public static void main(String[] args) {
+        Student s1= new Student("Ali", "Aliyev", 17,true,143,230104001);
+        Student s2=new Student("Shahmerdan","Balayev",19,true,170,210105003);
+        Student s3=new Student("Nigar","Caniyeva",18,false,181,220104001);
+        Student s4=new Student("Xanqiz","Qarali",21,false,133,200105021);
+        Student s5=new Student("Aysel","Quliyeva",20,false,120,240102017);
+        Teacher t1=new Teacher("Rashad","Xanbabazade",35,true,2000,101);
+        Teacher t2=new Teacher("Zerrin","Qemberova",30,false,1500,102);
+        Course java=new Course("Java","CS101",5);
+        Course python=new Course("Python","CS102",4);
+        Course cpp=new Course("C++","CS103",5);
+        Course calc1=new Course("Calculus I","MATH101",4);
+        Course calc2=new Course("Calculus II","MATH102",6);
+        Course phy1=new Course("Physics I","PHYS101",3);
+        Course phy2=new Course("Physics II","PHYS102",4);
+        Course hist=new Course("History","HIST101",3);
+        Course geo=new Course("Geography","GEOG101",2);
+        Course civilDef=new Course("Civil Defense","CIV101",2);
+        s1.takeCourse(java);
+        s1.takeCourse(calc2);
+        s1.takeCourse(hist);
+        s2.takeCourse(cpp);
+        s2.takeCourse(java);
+        s2.takeCourse(phy1);
+        s3.takeCourse(hist);
+        s3.takeCourse(phy2);
+        s3.takeCourse(calc2);
+        s4.takeCourse(calc1);
+        s4.takeCourse(phy1);
+        s4.takeCourse(python);
+        s5.takeCourse(phy2);
+        s5.takeCourse(geo);
+        s5.takeCourse(civilDef);
+        System.out.println("Students taking Java course: ");
+        for (Student student : java.getStudentsTakingCourse()) {
+            System.out.println(student.getName() + " " + student.getSurname());
+        }
+        System.out.println("Courses taken by " + s1.getName() + " " + s1.getSurname() + ": ");
+        for (Course course : s1.getCoursesTaken()) {
+            System.out.println(course.getCourseName());
+        }
+
+    }
+}
