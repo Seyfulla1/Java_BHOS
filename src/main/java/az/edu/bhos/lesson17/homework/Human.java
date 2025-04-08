20cf927a304c41f610c0d6d73424209a53381ed0
Seyfulla1 <160208902+Seyfulla1@users.noreply.github.com>
2025-04-08 16:06:49 +0400

    added getter methods

--- src/main/java/az/edu/bhos/lesson17/homework/Human.java
+++ src/main/java/az/edu/bhos/lesson17/homework/Human.java
@@ -3,10 +3,23 @@ package az.edu.bhos.lesson17.homework;
 public class Human {
     private final String name;
     private final String surname;
+    private boolean isMale;
     private int age;
-    public Human(String name, String surname, int age) {
+
+    public Human(String name, String surname, int age,boolean isMale) {
         this.name = name;
         this.surname = surname;
         this.age = age;
+        this.isMale = isMale;
     }
+    public String getName() {
+        return name;
+    }
+    public String getSurname() {
+        return surname;
+    }
+    public int getAge() {
+        return age;
+    }
+
 }
