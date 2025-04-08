9068958a15c9fa3253dcbe2c6c3e582ef97cc903
Seyfulla1 <160208902+Seyfulla1@users.noreply.github.com>
2025-04-08 16:10:09 +0400

    Implemented getter methods and methods to register and remove students

--- src/main/java/az/edu/bhos/lesson17/homework/Course.java
+++ src/main/java/az/edu/bhos/lesson17/homework/Course.java
@@ -7,6 +7,37 @@ public class Course {
     private String courseID;
     private int credits;
     private ArrayList<Student> studentsTakingCourse;
-    private ArrayList<Exam> examsForCourse;
-    
+
+    public Course(String courseName, String courseID, int credits) {
+        this.courseName = courseName;
+        this.courseID = courseID;
+        this.credits = credits;
+        this.studentsTakingCourse = new ArrayList<>();
+    }
+    public String getCourseName() {
+        return courseName;
+    }
+    public String getCourseID() {
+        return courseID;
+    }
+    public int getCredits() {
+        return credits;
+    }
+    public ArrayList<Student> getStudentsTakingCourse() {
+        return studentsTakingCourse;
+    }
+    public boolean registerStudent(Student student) {
+        if (student != null && !studentsTakingCourse.contains(student)) {
+            studentsTakingCourse.add(student);
+            return true;
+        }
+        return false;
+    }
+    public boolean removeStudent(Student student) {
+        if (student != null && studentsTakingCourse.contains(student)) {
+            studentsTakingCourse.remove(student);
+            return true;
+        }
+        return false;
+    }
 }
